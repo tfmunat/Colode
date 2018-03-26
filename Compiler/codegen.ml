9421573804eae737b2cb09117752f6688205891b
Willie Koomson <willie.koomson@gmail.com>
2018-03-26 18:41:47 -0400

    commented out unused

--- Compiler/codegen.ml
+++ Compiler/codegen.ml
@@ -12,22 +12,21 @@ let translate (statements, functions) =
 		and i8_t = L.i8_type context
 		and i1_t = L.i1_type  context (* used to represent boolean type *)
 		and float_t = L.double_type context
-		and void_t = L.void_type context
+		(* and void_t = L.void_type context *)
 		and char_t = L.i8_type context
 	in
 	(* Compound types *)
-	let list_t = fun (inner_typ: L.lltype) -> L.struct_type context [| L.pointer_type inner_typ; i32_t (*length*); i32_t (*capacity*)|] in
+	(* let list_t = fun (inner_typ: L.lltype) -> L.struct_type context [| L.pointer_type inner_typ; i32_t (*length*); i32_t (*capacity*)|] in *)
 	let string_t = L.struct_type context [| L.pointer_type char_t; i32_t (*length*); |] in 
-	let matrix_t = L.struct_type context [| L.pointer_type float_t; i32_t (*width*); i32_t (*height*) |]  in 
-	let image_t  = L.struct_type context [| i32_t (*width*); i32_t (* height *); L.pointer_type matrix_t; L.pointer_type matrix_t; L.pointer_type matrix_t; |] in
-	let pixel_t = L.vector_type float_t 4
-	in
+	(* let matrix_t = L.struct_type context [| L.pointer_type float_t; i32_t (*width*); i32_t (*height*) |]  in  *)
+	(* let image_t  = L.struct_type context [| i32_t (*width*); i32_t (* height *); L.pointer_type matrix_t; L.pointer_type matrix_t; L.pointer_type matrix_t; |] in *)
+	(* let pixel_t = L.vector_type float_t 4 in *)
 	(* Internal constants *)
-	let zero = L.const_int (L.i32_type context) 0 in
-	let one = L.const_int (L.i32_type context) 1 in
+	(* let zero = L.const_int (L.i32_type context) 0 in
+	let one = L.const_int (L.i32_type context) 1 in *)
 	(* Main module *)
 	let code_module = L.create_module context "Colode" in
-	let rec ltype_of_typ = function
+	(* let rec ltype_of_typ = function
 		  A.Int   -> i32_t
 		| A.Bool  -> i1_t
 		| A.Float -> float_t
@@ -38,10 +37,10 @@ let translate (statements, functions) =
 		| A.Image -> image_t
 		| A.Matrix -> matrix_t
 		| A.Pixel -> pixel_t
-	in
+	in *)
 	let print_t = L.function_type i32_t [| L.pointer_type char_t |] in
 	let print_func = L.declare_function "puts" print_t code_module in
-	let function_decls =
+	(* let function_decls =
 		let func_decl map fd =
 			let name = fd.sfname in
 			let formal_types = Array.of_list (List.map (fun (t,_) -> ltype_of_typ t) fd.sformals) in
@@ -49,7 +48,7 @@ let translate (statements, functions) =
 			StringMap.add name (L.define_function name func_type code_module, fd) map 
 		in
 		List.fold_left func_decl StringMap.empty functions
-	in
+	in *)
 	let lookup map name : L.llvalue = match StringMap.find_opt name map	with
 	  Some v -> v | None -> make_err ("Couldn't find " ^ name)
     in
@@ -75,9 +74,9 @@ let translate (statements, functions) =
 		(L.build_call print_func [|lo|] "" builder, map)
 	| _ -> make_err "Unimplemented"
 	in
-	let add_terminal builder fn = match L.block_terminator (L.insertion_block builder) with
+	(* let add_terminal builder fn = match L.block_terminator (L.insertion_block builder) with
 	  Some _ -> ()
-	| None -> ignore (fn builder) in
+	| None -> ignore (fn builder) in *)
 	let rec stmt map builder s = match s with
 	  SBlock sl -> List.fold_left (fun (b, m) s -> stmt m b s) (builder, map) sl
 	| SExpr e -> let (_, m) = (expr map builder e) in (builder, m)
