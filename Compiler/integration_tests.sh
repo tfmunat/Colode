38e18debc43c39a70ed6f82ce67fa89383c0fa73
Willie Koomson <willie.koomson@gmail.com>
2018-04-18 18:08:28 -0400

    integration tests

---
+++ Compiler/integration_tests.sh
@@ -0,0 +1,43 @@
+compiler="./toplevel.native"
+make clean
+make
+if [ $? -ne 0 ]; then
+	echo "Compilation failed."
+	exit 1
+fi
+success=0
+failure=0
+for t in ./Tests/*.cld; 
+	do
+		echo "================="
+		name=$(head -1 $t | sed 's/^\/\///')
+		echo $name
+		echo "-----------------"
+		$compiler -l $t > test.ll
+		res=$?
+		if echo $t | grep -qi "fail" ; then
+			if [ $res -eq 0 ]; then
+				echo "$name failed. Compilation should have failed."
+				failure=$((failure +1))
+			else
+				echo "$name succeeded. Compilation failed correctly."
+				success=$((success +1))
+			fi
+		else
+			expected=$(head -2 $t | tail -1 | sed 's/^\/\///')
+			output=$(lli test.ll)
+			echo $output
+			echo $output | grep -qa "$expected"
+			if [ $? -eq 0 ]; then
+				echo "$name succeeded; output matched expected: $expected"
+				success=$((success +1))
+			else
+				echo "$name failed; output did not match expected: $expected"
+				failure=$((failure +1))
+			fi
+		fi
+		rm -f test.ll
+		echo ""
+	done
+echo "================="
+echo "$success tests passed and $failure tests failure."
--- Compiler/tester.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for t in ../Tests/*.cld; do
-  echo "Running test $t"
-  ./toplevel.native < $t
-  echo "-----------------------------------------"
-done
